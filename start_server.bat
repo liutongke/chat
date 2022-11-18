6fa6b337fd7c9959439a61fccccf3d2854139628
keke <liutongke@live.com>
2022-11-18 23:44:07 +0800

    websocket、登录验证

--- start_server.bat
+++ start_server.bat
@@ -1,5 +1,7 @@
-SET dirPath=%~dp0
+@echo off
 
 docker build -t apiswoole ./
 
-docker run -i -t -p 9500:9500 -p 9501:9501 -p 9502:9502/udp --name swoole-100 -v %dirPath%:/var/www apiswoole
+SET dirPath=%~dp0
+
+docker run -i -t -p 9600:9500 -p 9601:9501 -p 9602:9502/udp --name swoole-bat-01 -v %dirPath%:/var/www apiswoole
